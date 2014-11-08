2c192eda316926e5849dd5f3dfe306a1f3033528
Kevin Growney <kevin@growney.net>
2014-11-08 18:49:44 +0000

    Converted collection view to angular

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -249,7 +249,41 @@ if (document.URL.match(/\/album.html/)) {
 // require("./album");
 // require("./profile");
 
-angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+var albumPicasso = {
+  name: 'The Colors',
+  artist: 'Pablo Picasso',
+  label: 'Cubism',
+  year: '1881',
+  albumArtUrl: '/images/album-placeholder.png',
+  songs: [
+    { name: 'Blue', length: '4:26'},
+    { name: 'Green', length: '3:14'},
+    { name: 'Red', length: '5:01'},
+    { name: 'Pink', length: '3:21'},
+    { name: 'Magenta', length: '2:15'}
+  ]
+};
+
+blocJams = angular.module('BlocJams', ['ui.router']);
+
+blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+  $locationProvider.html5Mode(true);
+
+  $stateProvider.state('landing', {
+    url: '/',
+    controller: 'Landing.controller',
+    templateUrl: '/templates/landing.html'
+  });
+
+  $stateProvider.state('collection', {
+    url: '/collection',
+    controller: 'Collection.controller',
+    templateUrl: '/templates/collection.html'
+  });
+}]);
+
+
+blocJams.controller('Landing.controller', ['$scope', function($scope) {
   $scope.subText = "Turn the music up!";
 
   $scope.subTextClicked = function() {
@@ -272,6 +306,14 @@ angular.module('BlocJams', []).controller('Landing.controller', ['$scope', funct
     shuffle($scope.albumURLs);
   };
 
+}]);
+
+blocJams.controller('Collection.controller', ['$scope', function($scope) {
+  $scope.albums = [];
+
+  for (var i = 0; i < 33; i++) {
+    $scope.albums.push(angular.copy(albumPicasso));
+  }
 }]);
 
 function shuffle(o){ //v1.0
