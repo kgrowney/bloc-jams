2ebd0272a245c41227bcf0b0f2ce1ab361698a52
Kevin Growney <kevin@growney.net>
2014-12-12 18:23:19 +0000

    Added volume control

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -363,6 +363,14 @@ blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope
 blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
   $scope.songPlayer = SongPlayer;
 
+  $scope.volumeClass = function() {
+    return {
+      'fa-volume-off': SongPlayer.volume == 0,
+      'fa-volume-down': SongPlayer.volume <= 70 && SongPlayer.volume > 0,
+      'fa-volume-up': SongPlayer.volume > 70
+    };
+  };
+
   SongPlayer.onTimeUpdate(function(event, time) {
     $scope.$apply(function() {
       $scope.playTime = time;
@@ -384,6 +392,7 @@ blocJams.service('SongPlayer', ['$rootScope', function($rootScope) {
     currentSong: null,
     currentAlbum: null,
     playing: false,
+    volume: 90,
 
     play: function() {
       this.playing = true;
@@ -419,6 +428,12 @@ blocJams.service('SongPlayer', ['$rootScope', function($rootScope) {
     },
     onTimeUpdate: function(callback) {
       return $rootScope.$on('sound:timeupdate', callback);
+    },
+    setVolume: function(volume) {
+      if (currentSoundFile) {
+        currentSoundFile.setVolume(volume);
+      }
+      this.volume = volume;
     },
     setSong: function(album, song) {
       if (currentSoundFile) {
